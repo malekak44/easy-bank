18c0106765ca6b3035a3662c9ddb0f1bc18272fb
malekak44 <malekak44@gmail.com>
2021-08-14 18:03:16 +0600

    hero section done

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,23 @@
-console.log('Jessica')
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
+        header.classList.remove('open'); // close menu
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    } else {
+        body.classList.add('noscroll');
+        header.classList.add('open'); //open menu
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
